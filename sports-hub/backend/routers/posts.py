caad815a3bd81aface90be885457f7c3e350b956
KunalSinghP <kunalsingh.ks384@gmail.com>
2026-03-22 22:05:44 +0530

    add posts system

--- sports-hub/backend/routers/posts.py
+++ sports-hub/backend/routers/posts.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import get_db
-from models.models import models
+from models import models
 
 router = APIRouter(prefix="/posts", tags=["posts"])
 
