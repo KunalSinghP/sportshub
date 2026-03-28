7cb3145e5e48c5f03dca1b94d2e44d70c8ee3270
KunalSinghP <kunalsingh.ks384@gmail.com>
2026-03-28 16:21:35 +0530

    cloud db

--- sports-hub/backend/database.py
+++ sports-hub/backend/database.py
@@ -2,11 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sportshub.db"
+import os
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL", 
+    "postgresql://sportshub_db_be4o_user:uhPXWIcAXI0hYz4OESb3Naq9o4D1o7C0@dpg-d73r0ipr0fns73cmjte0-a.oregon-postgres.render.com/sportshub_db_be4o"
 )
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
